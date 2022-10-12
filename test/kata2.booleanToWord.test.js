501ba3c1db5d9d944b6a500853e5055911875eb2
George Aldington <georgealdington@gmail.com>
2022-10-12 12:01:20 +0100

    created test for booleanToWord and wrote test that passed

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,10 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("returns 'yes' when passed the boolean value true", () => {
+    expect(booleanToWord(true)).toEqual("Yes");
+  });
+  it("returns 'no' when passed the boolean value false", () => {
+    expect(booleanToWord(false)).toEqual("No");
+  });
 });
