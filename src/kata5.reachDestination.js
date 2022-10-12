3be4c991d9bd7cc0032ae436a11e2af62f691f7d
George Aldington <georgealdington@gmail.com>
2022-10-12 16:16:06 +0100

    wrote test for reachDestinaiton function and wrote code to pass test

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const remainingTimeRounded = Math.ceil((distance / speed) * 2) / 2;
+  return `I should be there in ${remainingTimeRounded} hours.`;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,10 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours.");
+    expect(reachDestination(100, 15)).toEqual("I should be there in 7 hours.");
+    expect(reachDestination(13, 6)).toEqual("I should be there in 2.5 hours.");
+    expect(reachDestination(22, 10)).toEqual("I should be there in 2.5 hours.");
+  });
 });
