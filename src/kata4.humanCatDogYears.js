84e0813d1a63a3bc9846d89cd78446eab7e5b0ca
George Aldington <georgealdington@gmail.com>
2022-10-12 14:16:44 +0100

    finished humanCatDogYears test and code to pass test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,6 +1,29 @@
 const humanCatDogYears = (number) => {
-  const yearsArray = [number];
-  return yearsArray;
+  const yearsArray = [];
+  if (number === 0) {
+    const catYears = 0;
+    const dogYears = 0;
+    yearsArray.push(number, catYears, dogYears);
+    return yearsArray;
+  }
+  if (number === 1) {
+    const catYears = 15;
+    const dogYears = 15;
+    yearsArray.push(number, catYears, dogYears);
+    return yearsArray;
+  }
+  if (number === 2) {
+    const catYears = 15 + 9;
+    const dogYears = 15 + 9;
+    yearsArray.push(number, catYears, dogYears);
+    return yearsArray;
+  }
+  if (number >= 3) {
+    const catYears = 15 + 9 + (number - 2) * 4;
+    const dogYears = 15 + 9 + (number - 2) * 5;
+    yearsArray.push(number, catYears, dogYears);
+    return yearsArray;
+  }
 };
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,7 +1,35 @@
 const { humanCatDogYears } = require("../src");
 
 describe("humanCatDogYears", () => {
-  test("returns an array with the passed number value", () => {
+
+  xtest("returns an array with the passed number value", () => {
     expect(humanCatDogYears(10)).toEqual([10]);
+  });
+
+  xtest("returns an array with the passed number in cat years", () => {
+    expect(humanCatDogYears(10)).toEqual([56]);
+    expect(humanCatDogYears(3)).toEqual([28]);
+    expect(humanCatDogYears(0)).toEqual([0]);
+    expect(humanCatDogYears(1)).toEqual([15]);
+    expect(humanCatDogYears(2)).toEqual([24]);
+    expect(humanCatDogYears(17)).toEqual([84]);
+  });
+
+  xtest("returns an array with the passed number in cat years and dog years", () => {
+    expect(humanCatDogYears(10)).toEqual([56, 64]);
+    expect(humanCatDogYears(3)).toEqual([28, 29]);
+    expect(humanCatDogYears(0)).toEqual([0, 0]);
+    expect(humanCatDogYears(1)).toEqual([15, 15]);
+    expect(humanCatDogYears(2)).toEqual([24, 24]);
+    expect(humanCatDogYears(17)).toEqual([84, 99]);
+  });
+
+  test("returns an array with the passed number of human years, and the equivalent cat years and dog years", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+    expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+    expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+    expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    expect(humanCatDogYears(17)).toEqual([17, 84, 99]);
   });
 });
